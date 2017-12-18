70c583ae1c4f7dcfc84b29b574ae804764f6943d
Alaethia <truthcharis@gmail.com>
2017-12-18 14:38:50 -0600

    first commit

---
+++ client.js
@@ -0,0 +1,12 @@
+console.log('js sourced');
+$(document).ready(function() {
+$('#generate').on('click', append);
+
+});
+var clicks = 0;
+function append(){
+  for (var i = 0; i < clicks.length; i++) {
+  }
+    $('#clicked').append(clicks);
+}
+//I am really stuck on this assignment
