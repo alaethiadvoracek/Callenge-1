70afa5506c827f29fe09d283fc9f6641a3ac48db
Alaethia <truthcharis@gmail.com>
2017-12-18 19:23:40 -0600

    added some functions and notes

--- client.js
+++ client.js
@@ -1,12 +1,25 @@
 console.log('js sourced');
-$(document).ready(function() {
-$('#generate').on('click', append);
-
-});
 var clicks = 0;
+$(document).ready(function() { //wil run when DOM is loaded
+$('#generate').on('click', append);//append text with button
+$('#body').on('click', toggle());
+});//end ready function
+
+
+
+//delete button should remove container and contents
 function append(){
-  for (var i = 0; i < clicks.length; i++) {
-  }
-    $('#clicked').append(clicks);
-}
-//I am really stuck on this assignment
+  $('body').append('<div id ="div"></div');//append div
+  clicks++;
+  $('#div').append('<p id="ptag"></p>');//append <p>
+  $('#ptag').text(clicks);//append click count to DOM as text
+  //append two buttons to div
+  $('#div').append('<button id="swap" type="button">Swap</button>');
+  $('#div').append('<button id="delete" type="button">Delete</button>');
+}//end append function
+//generate button to have red background color
+//swap button should change to yellow background color use toggle()
+ function toggle() {
+   $('#swap').css("background-color:", "yellow");
+   $('#generate').css("backgroun-color:", "red");
+ }//end toggle function
